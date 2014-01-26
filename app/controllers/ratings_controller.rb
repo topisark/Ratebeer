63a062ca8b6210fd5b9e26df4f15256a4c8bfd43
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-01-26 18:52:41 +0200

    Arvostelun poistaminen

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -16,6 +16,12 @@ class RatingsController < ApplicationController
     redirect_to ratings_path
   end
 
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to ratings_path
+  end
+
   private
 # Use callbacks to share common setup or constraints between actions.
   def set_rating
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Ratebeer::Application.routes.draw do
   get 'ratings', to: 'ratings#index'
   get 'exercises', to: 'exercises#index'
   get 'exercises/exercise1', to: 'exercises#exercise1'
-  get 'ratings/new', to:'ratings#new'
-  post 'ratings', to: 'ratings#create'
+
+  resources :ratings, only: [:index, :new, :create, :destroy]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
