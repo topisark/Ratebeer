deacf6f9956cc7b12f80171c10e6ade75a61fee1
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-02 14:41:22 +0200

    Salasanalle vaatimuksia

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :beer_clubs, through: :memberships
   has_secure_password
   validates :username, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :password, format: {with: /\A(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,15}\z/, message: "requires one lowercase, one uppercase, one number and at least 4 characters."}
 
   def to_s
     return "#{self.username}"
