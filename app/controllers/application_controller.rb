a5d36e389fb315ff0333fed403c24e96cd1e2f00
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-08 13:17:08 +0200

    Kaunistelua

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
   def ensure_that_signed_in
     redirect_to signin_path, notice:'You need to be signed in!' if current_user.nil?
   end
+
+  def ensure_that_admin
+    redirect_to signin_path, notice:'You need to sign in with an admin account to do that!' unless current_user.admin?
+  end
 end
