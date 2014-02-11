6c38ba3f6c7c98aa8a10560d8f64fd7dc3773d8e
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-11 12:06:34 +0200

    Pubien testausta

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    BeermappingApi.stub(:places_in).with("kumpula").and_return(
+        [ Place.new(:name => "Oljenkorsi") ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+end
