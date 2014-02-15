848c212019489dd0eb7a3233e0a790fffb4bf973
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-15 15:52:08 +0200

    Greeting message for a new member

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -31,7 +31,7 @@ class MembershipsController < ApplicationController
 
     respond_to do |format|
       if @membership.save
-        format.html { redirect_to @membership, notice: 'Membership was successfully created.' }
+        format.html { redirect_to @membership.beer_club, notice: "Welcome to #{@membership.beer_club.name}, #{@membership.user.username}!" }
         format.json { render action: 'show', status: :created, location: @membership }
       else
         @beer_clubs = BeerClub.all
