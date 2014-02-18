dd8aa040b20e043fda02d8cde29bc9092b11d4d4
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-18 13:45:30 +0200

    Angularia vähän lissää

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function BeersController($scope, $http) {
+    $http.get('beers.json').success( function(data, status, headers, config) {
+        $scope.beers = data;
+    });
+
+    $scope.order = 'name';
+
+    $scope.click = function (order){
+        $scope.order = order;
+        console.log(order);
+    }
+
+    $scope.searchText = '';
+}
