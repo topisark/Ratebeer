4b5226d64cb0d57bdd5a5a97f87f6d7885c567d9
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-18 16:30:58 +0200

    Panimoidenkin listaus angularilla

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,3 +32,18 @@ function BeersController($scope, $http) {
     $scope.searchText = '';
 }
 
+function BreweriesController($scope, $http) {
+    $http.get('breweries.json').success( function(data, status, headers, config) {
+        $scope.breweries = data;
+    });
+
+    $scope.order = 'name';
+
+    $scope.click = function (order){
+        $scope.order = order;
+        console.log(order);
+    }
+
+    $scope.searchText = '';
+}
+
