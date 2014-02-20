6208a08d850c4197c366c6eac3174f3539034911
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-20 22:56:53 +0200

    Konfliktin ratkaisua?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Ratebeer::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
