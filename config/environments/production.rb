d0b7635b63e714e64fd183af3880afca9166e352
Topi Sarkkinen <topi.sarkkinen@cs.helsinki.fi>
2014-02-20 23:09:39 +0200

    Vielä yksi production-testaus

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Ratebeer::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.compress = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
