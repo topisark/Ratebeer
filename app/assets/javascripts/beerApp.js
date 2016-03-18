df0e6081697082134958e13d0c41f84df85c438e
topisark <topi.sarkkinen@cs.helsinki.fi>
2016-03-18 15:37:29 +0200

    Some progress with basic index views

--- app/assets/javascripts/beerApp.js
+++ app/assets/javascripts/beerApp.js
@@ -4,6 +4,14 @@ var app = angular.module('beerApp', ['ngRoute', 'templates'])
             .when('/beers', {
                 templateUrl: 'beers/index.html',
                 controller: 'BeersController'
+            })
+            .when('/breweries', {
+                templateUrl: 'breweries/index.html',
+                controller: 'BreweriesController'
+            })
+            .when('/users', {
+                templateUrl: 'users/index.html',
+                controller: 'UsersController'
             });
         $locationProvider.html5Mode(true);
     });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Ratebeer::Application.routes.draw do
 
   resources :beers, defaults: {format: :json}
+  resources :breweries, defaults: {format: :json}
+  resources :users, defaults: {format: :json}
   resources :sessions, only: [:new, :create]
 
   root 'application#index'
