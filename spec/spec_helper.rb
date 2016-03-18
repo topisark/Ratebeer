e53a88403a91d54beeb2bebe7fa8c383be815451
topisark <topi.sarkkinen@cs.helsinki.fi>
2016-03-18 13:15:36 +0200

    Refactored unnecessary repetition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,55 +1,68 @@
-require 'coveralls'
-Coveralls.wear!
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
-require 'simplecov'
-SimpleCov.start
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'capybara/rspec'
 require 'capybara/rails'
 require 'webmock/rspec'
+require 'coveralls'
+Coveralls.wear!
 I18n.enforce_available_locales = false
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
   config.use_transactional_fixtures = true
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
   config.order = "random"
-
   config.include Rails.application.routes.url_helpers
-
   config.include Capybara::DSL
+end
+
+def sign_in(credentials)
+  visit breweries_path
+  fill_in('username', with: credentials[:username])
+  fill_in('password', with: credentials[:password])
+  click_button('Sign in')
+end
+
+def create_beers_with_ratings(*scores, user)
+  scores.each do |score|
+    create_beer_with_rating(score, user)
+  end
+end
+
+def create_beer_with_rating(score, user)
+  beer = FactoryGirl.create(:beer)
+  FactoryGirl.create(:rating, score: score, beer: beer, user: user)
+  return beer
+end
+
+def create_beer_with_rating_for_brewery(brewery, score)
+  beer = FactoryGirl.create(:beer, brewery: brewery)
+  FactoryGirl.create(:rating, score: score, beer: beer)
+end
+
+def set_configurations_for_js_tests
+  before :all do
+    self.use_transactional_fixtures = false
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
+
+  before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  after :each do
+    DatabaseCleaner.clean
+  end
+
+  after :all do
+    self.use_transactional_fixtures = true
+  end
 end
