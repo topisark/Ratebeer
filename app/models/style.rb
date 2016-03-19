2a515626f30570a5c89e8f2e0f0639c410fd561e
topisark <topi.sarkkinen@cs.helsinki.fi>
2016-03-19 11:19:38 +0200

    Added the newly refactored class to Styles also

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,7 +1,8 @@
 class Style < ActiveRecord::Base
+  include RatingAverage
+  include Top
   has_many :beers
   has_many :ratings, through: :beers
-  include RatingAverage
 
   def to_s
     return self.name
